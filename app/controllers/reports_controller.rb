13a5bb8eeb6f80219f3e551f80326160186fc085
Brandon <bran.cucspace@gmail.com>
2018-06-26 21:32:20 +0000

    Reports module changes:modified the view of index Reports and show Report, adding the function to modify the read value from false to true at the hour of see the report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,7 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_admin
+  
   # GET /reports
   # GET /reports.json
   def index
@@ -10,6 +11,10 @@ class ReportsController < ApplicationController
   # GET /reports/1
   # GET /reports/1.json
   def show
+    if !@report.read
+      @report.read = true
+      @report.save
+    end
   end
 
   # GET /reports/new
@@ -70,5 +75,14 @@ class ReportsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def report_params
       params.require(:report).permit(:created_at, :finished_at, :user_id_id, :team_id_id)
+    end
+    
+    def require_admin
+      if (current_user and current_user.admin?) || (current_user and current_user.position.name.eql? "Manager" )
+      
+      else 
+        flash[:danger] = "Only managers users can perform that action"
+        redirect_to root_path
+      end
     end
 end
